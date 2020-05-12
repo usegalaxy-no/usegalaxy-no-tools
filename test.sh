9985968346bb660cf140dc8d3e49a1e4536f5cd8
Torfinn Nome <torfinn.nome@gmail.com>
2020-05-12 11:35:22 +0200

    Replace Jenkins specific variables

---
+++ test.sh
@@ -0,0 +1,33 @@
+MODE=install
+PRODUCTION_URL=https://test.usegalaxy.no
+PRODUCTION_API_KEY=a4d452413e8fce11558132bf3e9b7660
+
+
+for TOOL_FILE in tmp/*; do
+    FILE_NAME=$(basename $TOOL_FILE)
+
+    TOOL_REF=$(echo $FILE_NAME | cut -d'.' -f 1);
+    TOOL_NAME=$(echo $TOOL_REF | cut -d '@' -f 1);
+    REQUESTED_REVISION=$(echo $TOOL_REF | cut -d '@' -f 2); # TODO: Parsing from file name for revision and tool name is not good.  Fix.
+    OWNER=$(grep -oE "owner: .*$" "$TOOL_FILE" | cut -d ':' -f 2 | xargs);
+    TOOL_SHED_URL=$(grep -oE "tool_shed_url: .*$" "$TOOL_FILE" | cut -d ':' -f 2 | xargs);
+    [ ! $TOOL_SHED_URL ] && TOOL_SHED_URL="toolshed.g2.bx.psu.edu"; # default value
+    SECTION_LABEL=$(grep -oE "tool_panel_section_label: .*$" "$TOOL_FILE" | cut -d ':' -f 2 | xargs);
+
+    # If either [FORCE] in the commit message or [SKIP_TESTS] in the file header, skip tests for this tool
+    if [ "$(grep '\[SKIP_TESTS\]' $TOOL_FILE)" ] || [ $FORCE = 1 ]; then
+      SKIP_TESTS=1
+    else
+      SKIP_TESTS=0
+    fi
+
+    # Find out whether tool/owner combination already exists on galaxy.  This makes no difference to the installation process but
+    # is useful for the log
+    TOOL_IS_NEW="False"
+    if [ $MODE == "install" ]; then
+      TOOL_IS_NEW=$(python scripts/is_tool_new.py -g $PRODUCTION_URL -a $PRODUCTION_API_KEY -n $TOOL_NAME -o $OWNER)
+    fi
+
+    unset STAGING_TESTS_PASSED PRODUCTION_TESTS_PASSED; # ensure these values do not carry over from previous iterations of the loop
+
+done
